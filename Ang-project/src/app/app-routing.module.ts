3ba744ce851e8c800947fbc8d8d7b6eab19b1964
Nit <niteesh91199@gmail.com>
2022-01-07 22:55:20 +0530

    new application

---
+++ Ang-project/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+import { OurProductsComponent } from './our-products/our-products.component';
+import { TermsComponent } from './terms/terms.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"Home",component:HomeComponent},
+  {path:"Products",component:OurProductsComponent},  
+  {path:"About",component:AboutComponent},
+  {path:"Contacts",component:ContactsComponent},
+  {path:"Terms",component:TermsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, OurProductsComponent,
+   TermsComponent, AboutComponent, ContactsComponent]
